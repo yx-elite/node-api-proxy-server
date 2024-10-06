931d7ef8185c37ac0e74b90ae8539619acddbfc7
Yi Xian <exian0716@gmail.com>
2024-10-06 22:32:03 +0800

    refactor: Rename images routes for clarity

--- controller/images.controller.js
+++ controller/images.controller.js
@@ -1,9 +1,9 @@
-import { imageGenerationsRoute, sdGenerationsRoute, fluxGenerationsRoute } from "../constants.js";
+import { imageGenerationsRoute, chatCompletionsRoute } from "../constants.js";
 import { nonStreamRequest } from "./request.js";
 
 
 export const imageGenerations = async (req, res) => nonStreamRequest(req, res, imageGenerationsRoute);
 
-export const sdGenerations = async (req, res) => nonStreamRequest(req, res, sdGenerationsRoute);
+export const sdGenerations = async (req, res) => nonStreamRequest(req, res, chatCompletionsRoute);
 
-export const fluxGenerations = async (req, res) => nonStreamRequest(req, res, fluxGenerationsRoute)
+export const fluxGenerations = async (req, res) => nonStreamRequest(req, res, chatCompletionsRoute)
--- route/images.route.js
+++ route/images.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 
-import {imageGenerationsRoute, sdGenerationsRoute, fluxGenerationsRoute } from "../constants.js";
-import {fluxGenerations, imageGenerations, sdGenerations} from "../controller/images.controller.js";
+import { imageGenerationsRoute, sdGenerationsRoute, fluxGenerationsRoute } from "../constants.js";
+import { fluxGenerations, imageGenerations, sdGenerations } from "../controller/images.controller.js";
 
 
 const router = express.Router();
 
 router.post(imageGenerationsRoute, imageGenerations);    // 'api/v1/images/generations' (Dall-e)
-router.post(sdGenerationsRoute, sdGenerations);          // 'api/v1/chat/completions' (Stable Diffusion)
-router.post(fluxGenerationsRoute, fluxGenerations);      // 'api/v1/chat/completions' (Flux)
+router.post(sdGenerationsRoute, sdGenerations);          // 'api/v1/sd/generations' (Stable Diffusion)
+router.post(fluxGenerationsRoute, fluxGenerations);      // 'api/v1/flux/generations' (Flux)
 
 export default router
